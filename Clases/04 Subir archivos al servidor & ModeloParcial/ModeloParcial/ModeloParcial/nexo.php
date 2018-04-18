7d26b3e2910adf596165dadca68668611d58eacc
nlsargit <aramayo.fabian@gmail.com>
2018-04-18 00:46:25 -0300

    Modificar agregado
    Para modificar se tiene que pasar el objeto, si tiene el mismo legajo se cambia el nombre o la foto.

---
+++ Clases/04 Subir archivos al servidor & ModeloParcial/ModeloParcial/ModeloParcial/nexo.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once ("entidades/alumno.php");
+require_once ("entidades/archivo.php");
+//var_dump($_POST);
+var_dump($_POST);
+var_dump($_FILES);
+
+
+
+$queHago = isset($_POST['queHago']) ? $_POST['queHago'] : NULL;
+
+switch($queHago){
+
+
+	case "Subir":
+	case "modificar":
+		
+
+		$respuestaDeSubir = Archivo::Subir();
+
+		if(!$respuestaDeSubir["Exito"]){
+			echo "error " .$respuestaDeSubir["Mensaje"];
+			break;
+		}
+
+		$legajo = isset($_POST['legajo']) ? $_POST['legajo'] : NULL;
+		$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : NULL;
+		$archivo = $respuestaDeSubir["PathTemporal"];
+
+		$p = new alumno($nombre, $legajo,$archivo);
+
+		echo "Bien " ;
+		/*
+		$codBarra = isset($_POST['codBarra']) ? $_POST['codBarra'] : NULL;
+		$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : NULL;
+		$archivo = $res["PathTemporal"];
+*/
+	//	$p = new Producto($codBarra, $nombre, $archivo);
+
+		// SUBIR
+		if ($queHago === "Subir") {
+			if(!alumno::Guardar($p)){
+				echo "Error al generar archivo";
+				break;
+			}	
+		}
+		
+	
+		// MODIFICAR
+		if ($queHago == "modificar") {
+			if (!alumno::Modificar($p)) {
+				echo "Error no se pudo modificar el archivo";
+				break;
+			}
+		}
+		
+	
+		
+		break;
+		
+	case "eliminar":
+		$legajo = isset($_POST['legajo']) ? $_POST['legajo'] : NULL;
+	
+		if(!alumno::Eliminar($legajo)){
+			$mensaje = "Lamentablemente ocurrio un error y no se pudo escribir en el archivo.";
+		}
+		else{
+			$mensaje = "El archivo fue escrito correctamente. Alumno eliminado CORRECTAMENTE!!!";
+		}
+	
+		echo $mensaje;
+		
+		break;
+		
+	default:
+		echo ":(";
+}
+
+?>
